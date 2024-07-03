d47a3fcc268976176abf8d074317168b9e9ee64f
Mosab Mohamed <129628518+mosabmohamed3@users.noreply.github.com>
2024-07-03 21:57:25 +0300

    Time: 132 ms (45.94%), Space: 47.3 MB (78.07%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        if(nums == null || nums.Length < 2)
+            return null;
+            
+        for (int a = 0; a < nums.Length; a++)
+        {
+            for (int b = a + 1; b < nums.Length; b++)
+            {
+                if (nums[a] + nums[b] == target)
+                    return new int[] { a, b };
+            }
+        }
+        return null;
+    }
+}
