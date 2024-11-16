d9093572c973f011aead7af0bc28adbb4ddaedff
Mosab Mohamed <mosabmohamed361@gmail.com>
2024-11-16 11:39:13 +0200

    Time: 300 ms (49.82%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select
+    machine_id,
+    Round((sum(case when activity_type = 'end' then timestamp else 0 end) -
+    sum(case when activity_type = 'start' then timestamp else 0 end))
+    / count(distinct process_id), 3) as processing_time
+from activity
+group by machine_id
