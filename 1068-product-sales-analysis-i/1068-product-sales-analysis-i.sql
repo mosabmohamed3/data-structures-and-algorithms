432f9612b61cb216086f9766c31bc0a8d233370a
Mosab Mohamed <mosabmohamed361@gmail.com>
2025-01-25 15:25:08 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,7 +1,4 @@
 /* Write your T-SQL query statement below */
-select
-     p.product_name, s.year, s.price
-from
-     Sales as s
-inner join
-     Product p on s.product_id = p.product_id 
+SELECT Product.product_name, Sales.year, Sales.price
+FROM Sales
+INNER JOIN Product ON Sales.product_id = Product.product_id
