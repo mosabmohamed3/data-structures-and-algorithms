4c41865faa740399778da8f97ebb0ee49cff560a
Mosab Mohamed <mosabmohamed361@gmail.com>
2025-03-10 00:23:48 +0200

    Time: 706 ms (80.13%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -5,6 +5,8 @@ SELECT
     COUNT(e.student_id) AS attended_exams
 from Students s
 cross join Subjects su
-left join Examinations e on s.student_id = e.student_id and su.subject_name = E.subject_name
+left join Examinations e 
+    on s.student_id = e.student_id 
+    and su.subject_name = e.subject_name
 GROUP BY s.student_id, s.student_name, su.subject_name
 ORDER BY s.student_id, s.student_name, su.subject_name
