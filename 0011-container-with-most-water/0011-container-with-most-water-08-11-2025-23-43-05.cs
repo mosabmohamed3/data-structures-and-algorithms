efb3fdf2ae8a93ebe369c5f6c7ef07215a2cff83
Mosab Mohamed <mosabmohamed361@gmail.com>
2025-08-11 23:43:07 +0300

    Time: 1 ms (100%), Space: 63.4 MB (39.86%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water-08-11-2025-23-43-05.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MaxArea(int[] height) {
+        int res = 0;
+        int l = 0, r = height.Length -1;
+
+        while(l<r)
+        {
+            res = Math.Max(res, Math.Min(height[l], height[r]) * (r-l));
+
+            if(height[l] <= height[r])
+                l++;
+            else
+                r--;
+        }
+
+        return res;
+    }
+}
