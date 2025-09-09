4ff2dc2417f78ef2d195e4dd9916ca51b31ed729
Mosab Mohamed <mosabmohamed361@gmail.com>
2025-09-09 14:48:39 +0300

    Time: 2 ms (66.16%), Space: 54.5 MB (63.14%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cs
@@ -0,0 +1,41 @@
+public class MinStack {
+    private Stack<int> stack;
+    private Stack<int> minStack;
+
+    public MinStack() {
+        stack = new Stack<int>();
+        minStack = new Stack<int>();
+    }
+    
+    public void Push(int val) {
+        stack.Push(val);
+
+        if(minStack.Count == 0 || val <= minStack.Peek())
+            minStack.Push(val);
+        else
+            minStack.Push(minStack.Peek());
+    }
+    
+    public void Pop() {
+        if(stack.Count > 0)
+        {
+            stack.Pop();
+            minStack.Pop();
+        }
+    }
+    
+    public int Top() =>
+        stack.Peek();
+    
+    public int GetMin() =>
+        minStack.Peek();
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.Push(val);
+ * obj.Pop();
+ * int param_3 = obj.Top();
+ * int param_4 = obj.GetMin();
+ */
