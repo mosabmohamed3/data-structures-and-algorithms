2caee4d1ed3e578592b6fccc526778cee8ae1538
Mosab Mohamed <mosabmohamed361@gmail.com>
2025-09-14 06:42:43 +0300

    Time: 2 ms (21.32%), Space: 50.4 MB (32.32%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses-09-14-2025-06-42-42.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public IList<string> GenerateParenthesis(int n) {
+        var result = new List<string>();
+
+        var stack = new Stack<(string current, int open, int close)>();
+        stack.Push(("", 0,0));
+
+        while(stack.Count > 0)
+        {
+            var (current, open, close) = stack.Pop();
+
+            if (current.Length == n * 2)
+                result.Add(current);
+            
+            if (open < n)
+                stack.Push((current + '(', open+1, close));
+
+            if (close < open)
+                stack.Push((current + ')', open, close+1));
+        }
+
+        return result;
+    }
+}
