45f011803aa1acf40a0d0620081a8049f38e88f9
Mosab Mohamed <mosabmohamed361@gmail.com>
2025-11-06 15:20:51 +0200

    Time: 2 ms (92.73%), Space: 48.9 MB (84.36%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree-11-06-2025-15-20-49.cs
@@ -0,0 +1,55 @@
+// public class Solution {
+//     public bool IsSubtree(TreeNode root, TreeNode subRoot) {
+//         if (root == null)
+//             return false;
+
+//         // Use DFS to traverse the main tree
+//         return DFS(root, subRoot);
+//     }
+
+//     public bool DFS (TreeNode root, TreeNode subRoot)
+//     {
+//         if (root == null) return false;
+
+//         if(isIt(root, subRoot)) return true;
+
+//         return DFS(root.left, subRoot) || DFS(root.right, subRoot);
+//     }
+
+//     public bool isIt(TreeNode root, TreeNode sub)
+//     {
+//         if (root.val == sub.val &&
+        
+//             root.left.val == sub.left.val &&
+//             root.right.val == sub.right.val &&
+
+//             root.left.left == sub.left.left &&
+//             root.left.right == sub.left.right &&     
+                
+//             root.right.left == sub.right.left &&
+//             root.right.right == sub.right.right
+//         )
+//             return true;
+//         return false;
+//     }
+// }
+
+public class Solution {
+    public bool IsSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+        
+        return 
+            IsSameTree(root, subRoot) ||
+            IsSubtree(root.left, subRoot) ||
+            IsSubtree(root.right, subRoot);
+    }
+
+    public bool IsSameTree(TreeNode root, TreeNode subRoot)
+    {
+        if (root == null && subRoot == null) return true;
+        if (root == null || subRoot == null) return false;
+        if (root.val != subRoot.val) return false;
+
+        return IsSameTree(root.left, subRoot.left) && IsSameTree(root.right, subRoot.right);
+    }
+}
