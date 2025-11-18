7c1d362d1671ab6267bcbb1f51bbec02b8fc382f
Mosab Mohamed <mosabmohamed361@gmail.com>
2025-11-18 20:54:03 +0200

    Time: 7 ms (5%), Space: 46.6 MB (67.03%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst-11-18-2025-20-54-01.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public List<int> vals = new List<int>();
+
+    public int KthSmallest(TreeNode root, int k) {
+        Dfs(root);
+
+        vals.Sort();
+        return vals[k-1];
+    }
+
+    public void Dfs(TreeNode node)
+    {
+        if (node is null) return;
+
+        vals.Add(node.val);
+
+        Dfs(node.left);
+        Dfs(node.right);
+    }
+}
