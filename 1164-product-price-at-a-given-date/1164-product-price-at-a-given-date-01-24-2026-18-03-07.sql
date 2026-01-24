1daec68d5e23742a1cfa7eb210308f3162ed72af
Mosab Mohamed <mosabmohamed361@gmail.com>
2026-01-24 18:03:09 +0200

    Time: 987 ms (29.75%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date-01-24-2026-18-03-07.sql
@@ -0,0 +1,19 @@
+/* Write your T-SQL query statement below */
+with AllProducts as(
+    select distinct product_id
+    from Products
+),
+LastChanges as(
+    select
+        product_id,
+        new_price,
+        row_number() over (partition by product_id order by change_date desc) as rn
+    from Products
+    where change_date <= '2019-08-16'
+)
+select
+     AllProducts.product_id,
+     COALESCE(LastChanges.new_price, 10) as price
+from AllProducts
+left join LastChanges
+    on AllProducts.product_id = LastChanges.product_id and LastChanges.rn = 1;
